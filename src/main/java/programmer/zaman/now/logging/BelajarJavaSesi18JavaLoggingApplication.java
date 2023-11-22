4d88b83ce5f55bc98d6b76abd745079e46ba8530
dede-irwanto <id.dedeirwanto@gmail.com>
2023-11-23 00:16:22 +0800

    first commit

---
+++ src/main/java/programmer/zaman/now/logging/BelajarJavaSesi18JavaLoggingApplication.java
@@ -0,0 +1,13 @@
+package programmer.zaman.now.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaSesi18JavaLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaSesi18JavaLoggingApplication.class, args);
+	}
+
+}
